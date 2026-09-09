8ad45c54278ccb4a6ab9f0926b14a44c52ff51b8
AMIRA DEL ROCÍO CHOQUE CARRASCO <amira.choque@ucb.edu.bo>
2026-09-09 15:51:47 -0400

    feat: implementacion de presentar.js

--- src/presenter.js
+++ src/presenter.js
@@ -4,6 +4,7 @@ const form = document.querySelector("#totalizador-form");
 const cantidadInput = document.querySelector("#cantidad");
 const precioInput = document.querySelector("#precio");
 const estadoSelect = document.querySelector("#estado");
+const categoriaSelect = document.querySelector("#categoria");
 const btnCancelar = document.querySelector("#btn-cancelar");
 const resultadoDiv = document.querySelector("#resultado-div");
 
@@ -13,15 +14,22 @@ form.addEventListener("submit", (event) => {
     const cantidad = Totalizador.validarCantidad(cantidadInput.value);
     const precio = Totalizador.validarPrecio(precioInput.value);
     const estado = Totalizador.obtenerEstado(estadoSelect.value);
+    const categoria = Totalizador.obtenerCategoria(categoriaSelect.value);
 
     const neto = Totalizador.calcularPrecioNeto(cantidad, precio);
-    const impuesto = Totalizador.calcularImpuestoEstado(neto, estado);
-    const pctDescuento = Totalizador.obtenerPorcentajeDescuentoMonto(neto);
-    const montoDescuento = Totalizador.calcularDescuentoMonto(neto);
+    const pctDescuentoMonto = Totalizador.obtenerPorcentajeDescuentoMonto(neto);
+    const descMonto = Totalizador.calcularDescuentoMonto(neto);
+    const descCat = Totalizador.calcularDescuentoCategoria(neto, categoria);
+    
+    const impEstado = Totalizador.calcularImpuestoEstado(neto, estado);
+    const impCat = Totalizador.calcularImpuestoCategoria(neto, categoria);
 
     resultadoDiv.innerHTML = `
       <p>Precio neto (${cantidad} * $${precio.toFixed(2)}): $${neto.toFixed(2)}</p>
-      <p>Impuesto para ${estado}: $${impuesto.toFixed(2)}</p>
+      <p>Descuento por monto (${(pctDescuentoMonto * 100)}%): -$${descMonto.toFixed(2)}</p>
+      <p>Descuento categoría (${categoria}): -$${descCat.toFixed(2)}</p>
+      <p>Impuesto de estado (${estado}): $${impEstado.toFixed(2)}</p>
+      <p>Impuesto categoría (${categoria}): $${impCat.toFixed(2)}</p>
     `;
   } catch (error) {
     resultadoDiv.innerHTML = `<p style="color:red;">Error: ${error.message}</p>`;
